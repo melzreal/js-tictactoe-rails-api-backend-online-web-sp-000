766ed841a639acc204aae530c5fca8c4d27f855f
Melissa Real <melissa.real@hotmail.com>
2019-07-02 20:37:08 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,17 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def index
-      games = Game.all 
-      render json: games
+    games = Game.all 
+    render json: games
   end
 
   def show
+    game = Game.find(params[:id])
+    render json: game
   end
 
   def create
+    game = Game.new 
   end
 
   def update
