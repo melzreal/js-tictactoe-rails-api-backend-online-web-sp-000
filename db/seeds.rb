400b486fcdde5a5d8b89691ef5eb641f886786f6
Melissa Real <melissa.real@hotmail.com>
2019-07-02 20:56:18 +0000

    Automatically backed up by Learn

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+g1 = Game.create(:state => ["X", "O",  "X", "", "", "", "", "", ""])
+g2 = Game.create(:state => ["X", "X",  "", "O", "", "", "", "", ""])
+g3 = Game.create(:state => ["X", "O",  "X", "O", "", "", "X", "", ""])
